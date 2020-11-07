3736d0a8daff4c30a36fec156ede1cfd8c812f95
xdcool.0 <xdcool.0@gmail.com>
2020-11-07 13:51:16 +0800

    11/7

--- proj2.cpp
+++ proj2.cpp
@@ -5,20 +5,68 @@
 
 using namespace std;
 
+class flora {
+private:
+    char line[2056]{0}, * temp = NULL, * slices = NULL;
+    int nraw = 0, ncolumn = 0;
+public:
+    int*** floor = NULL;
+    /*flora() {
+        
+    }*/
+
+    void load_floor(char* path) {
+        ifstream input_file(path);
+        if (!input_file) cout << "load input file failed" << endl;
+
+        input_file.getline(line, 16);
+        temp = strtok_s(line, " ", &slices);
+        nraw = stoi(temp);
+        temp = strtok_s(NULL, " ", &slices);
+        ncolumn = stoi(temp);
+        cout << nraw << " " << ncolumn << endl;
+
+        floor = new int** [nraw]; //floor[nraw][ncolumn][3]
+        for (int i = 0; i < nraw; i++) {
+            floor[i] = new int *[ncolumn];
+            int index = 0;
+            floor[i][index] = new int[3]{0};
+
+            input_file.getline(line, 2056);
+            temp = strtok_s(line, " ", &slices);
+            
+            floor[i][index][0] = 1;
+            index++;
+
+            while (temp = strtok_s(NULL, " ", &slices)) {
+                //temp = strtok_s(NULL, " ", &slices);
+                if (temp[0] == '1') {
+                    floor[i][index][0] = 1;
+                }
+                else if (temp[0] == '0') {
+                    floor[i][index][0] = 0;
+                }
+                else {
+                    floor[i][index][0] = -1;
+                    floor[i][index][2] = 1;
+                }
+            }
+        }
+
+    }
+
+    void print_floor() {
+        
+    }
+};
+    
+
+
 int main(int argc, char* argv[])
 {
     cout << "Hello World!\n";
-    char line[2056], * temp, *slices = NULL;
-    int x = 0, y = 0;
 
-    ifstream input_file(argv[1]);
-    if (!input_file) cout << "load input file failed" << endl;
+    
 
-    input_file.getline(line, 16);
-    temp = strtok_s(line, " ", &slices);
-    x = stoi(temp);
-    temp = strtok_s(NULL, " ", &slices);
-    y = stoi(temp);
-    cout << x << " " << y << endl;
 
 }
